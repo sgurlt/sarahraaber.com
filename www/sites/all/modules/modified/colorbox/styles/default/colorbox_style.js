6ea15333d184da9383e903fd394cbc825f10a5f1
gurlt <gurlt@brightsolutions.de>
2014-11-17 11:37:15 +0100

    move colorbox to modified

---
+++ www/sites/all/modules/modified/colorbox/styles/default/colorbox_style.js
@@ -0,0 +1,35 @@
+(function ($) {
+
+Drupal.behaviors.initColorboxDefaultStyle = {
+  attach: function (context, settings) {
+    $(context).bind('cbox_complete', function () {
+      // Only run if there is a title.
+      if ($('#cboxTitle:empty', context).length == false) {
+        $('#cboxLoadedContent img', context).bind('mouseover', function () {
+          $('#cboxTitle', context).slideDown();
+        });
+        $('#cboxOverlay', context).bind('mouseover', function () {
+          $('#cboxTitle', context).slideUp();
+        });
+      }
+      else {
+        $('#cboxTitle', context).hide();
+      }
+      var colorbox_original_image = Drupal.settings.colorbox.colorbox_original_image;
+      if(colorbox_original_image == '1') {
+        if ($('#cboxLoadedContent > img').attr('src')) {
+          var fullHref = $('#cboxLoadedContent > img').attr('src').replace(/styles\/large\/public\//,'');
+          var fullLink = $('<a/>');
+          fullLink.attr('href', fullHref);
+          fullLink.attr('target', 'new');
+          fullLink.attr('title', 'Right click to download');
+          fullLink.addClass("download_link");
+          $('#cboxClose').once('cboxClose').before(fullLink);
+          $('.download_link').once('download_link').wrap('<div id="cboxDownload"></div>')
+          }
+        }
+    });
+  }
+};
+
+})(jQuery);
