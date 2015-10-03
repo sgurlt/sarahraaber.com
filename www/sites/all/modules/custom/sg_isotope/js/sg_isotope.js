c5626d7203f8a1a051a8ed945514faa9b81a8719
gurlt <gurlt@brightsolutions.de>
2015-10-03 13:53:46 +0200

    Try to fix isotope layout issue.

--- www/sites/all/modules/custom/sg_isotope/js/sg_isotope.js
+++ www/sites/all/modules/custom/sg_isotope/js/sg_isotope.js
@@ -1,27 +1,44 @@
 (function ($, window, undefined) {
 
-
   // Initial masonry
   Drupal.behaviors.initialMasonry = {
     attach: function (context, settings) {
       $('body').once(function() {
         var pathname = window.location.pathname;
         if(pathname == '/artwork') {
-          var container = $('.view-artwork');
-          $(container).imagesLoaded(function() {
-            $('.page-artwork .view-artwork .view-content').css('opacity','1');
-            $('.page-artwork .view-artwork .view-content').css('pointer-events','initial');
-            $('.page-artwork .view-artwork ').css('background-image','none');
-            $(container).isotope({
-              // options
-              itemSelector : '.views-row'
-            });
+
+          // Disable page scrolling to prevent layout bugs.
+          //$('html, body').css({
+          //  'overflow': 'hidden',
+          //  'height': '100%'
+          //});
+
+          var $container = $('.view-artwork').isotope({
+            itemSelector : '.views-row'
           });
+
+          // layout Isotope after each image loads
+          $container.imagesLoaded().progress( function() {
+            $container.isotope('layout');
+              $('.page-artwork .view-artwork .view-content').css('opacity','1').css('pointer-events','initial');
+              $('.page-artwork .view-artwork ').css('background-image','none');
+          });
+
+          //var imgLoad = imagesLoaded($container);
+          //imgLoad.on( 'always', function( instance ) {
+          //  $('.page-artwork .view-artwork .view-content').css('opacity','1').css('pointer-events','initial');
+          //  $('.page-artwork .view-artwork ').css('background-image','none');
+          //  $container.isotope({
+          //    // options
+          //    itemSelector : '.views-row'
+          //  });
+          //});
+
 
 
           $('#filters a').click(function(){
             var selector = $(this).attr('data-filter');
-            container.isotope({ filter: selector });
+            $container.isotope({ filter: selector });
             return false;
           });
         }
